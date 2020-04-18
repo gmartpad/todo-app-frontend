c335ad514f95435bdea320df24298d2d9da6bdb9
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-04-18 19:58:04 -0300

    Componente Menu

---
+++ src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Todo extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
