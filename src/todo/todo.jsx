271b1cd29458b4ba5e8c0a1530186399251dda28
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-04-19 20:20:39 -0300

    Evento Adicionar

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,12 +4,23 @@ import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
 export default class Todo extends React.Component{
+    
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log('Add')
+    }
+    
+    
     render()
     {
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" ></PageHeader>
-                <TodoForm/>
+                <TodoForm handleAdd={this.handleAdd}/>
                 <TodoList/>
             </div>
         )
