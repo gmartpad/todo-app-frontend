d80000dd60cca37dc24d8971265e9dbc94a7db0f
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-04-19 22:21:02 -0300

    Consulta e Exclusão de TODOs

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -16,6 +16,14 @@ export default class Todo extends React.Component{
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleChange(e) {
@@ -25,9 +33,13 @@ export default class Todo extends React.Component{
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou!'))
+            .then(resp => this.refresh())
     }
     
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
     
     render()
     {
@@ -37,7 +49,8 @@ export default class Todo extends React.Component{
                 <TodoForm description={this.state.description} 
                 handleChange={this.handleChange}
                 handleAdd={this.handleAdd}/>
-                <TodoList/>
+                <TodoList list={this.state.list}
+                handleRemove={this.handleRemove}/>
             </div>
         )
     }
